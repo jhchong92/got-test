ca4c600d90059142223590d4cd22fe0d41f379d1
Jeunn Hao <jh@poetfarmer.com>
2020-03-26 22:17:51 +0800

    uncomment codes

--- index.js
+++ index.js
@@ -74,95 +74,94 @@ const handler = {
   }
 }
 
-// 
-// console.log('async getPosts START')
-// got('posts', impl.options())
-//   .json()
-//   .then((response) => {
-//     console.log(`GET /posts - obtained ${response.length} posts`)
-//   })
-//   .catch((error) => {
-//     handler.error('posts', 'GET', error)
-//   })
-//   .finally(() => {
-//     console.log('async getPosts END')
-// })
+console.log('async getPosts START')
+got('posts', impl.options())
+  .json()
+  .then((response) => {
+    console.log(`GET /posts - obtained ${response.length} posts`)
+  })
+  .catch((error) => {
+    handler.error('posts', 'GET', error)
+  })
+  .finally(() => {
+    console.log('async getPosts END')
+})
 
-// console.log('async getPostz START')
-// got('postz', impl.options())
-//   .json()
-//   .then((response) => {
-//     console.log(`GET /posts - obtained ${response.length} posts`)
-//   })
+console.log('async getPostz START')
+got('postz', impl.options())
+  .json()
+  .then((response) => {
+    console.log(`GET /posts - obtained ${response.length} posts`)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  .finally((err) => {
+    console.log('async getPostz START')
+  })
+
+const awaitWithError = async () => {
+  console.log('awaitWithError START')
+  
+  try {
+    await got('postz', impl.options())
+  } catch(err) {
+    handler.error('postz', 'GET', err)
+  }
+  console.log('awaitWithError END')
+}
+awaitWithError()
+
+const getMultiplePostsPromise = async () => {
+  console.log('getMultiplePostsPromise')
+  
+  console.log('GET multiple posts with Promise START')
+  const results = await Promise.all([
+    got('posts/1', impl.options()).json(),
+    got('posts/2', impl.options()).json(),
+  ])
+  console.log('Results from getMultiplePostsPromise:-\n', results)
+  console.log('GET multiple posts with Promise END')
+}
+getMultiplePostsPromise()
+
+// const simulateServerError = async () => {
+//   console.log('simulateServerError - This might take a while...')
+//   got.put('postz', impl.options())
 //   .catch((err) => {
 //     console.log(err)
+//     handler.error('postz', 'PUT',  err)
 //   })
-//   .finally((err) => {
-//     console.log('async getPostz START')
-//   })
-
-// const awaitWithError = async () => {
-//   console.log('awaitWithError START')
-  
-//   try {
-//     await got('postz', impl.options())
-//   } catch(err) {
-//     handler.error('postz', 'GET', err)
-//   }
-//   console.log('awaitWithError END')
 // }
-// awaitWithError()
+// simulateServerError()
 
-// const getMultiplePostsPromise = async () => {
-//   console.log('getMultiplePostsPromise')
-  
-//   console.log('GET multiple posts with Promise START')
-//   const results = await Promise.all([
-//     got('posts/1', impl.options()).json(),
-//     got('posts/2', impl.options()).json(),
-//   ])
-//   console.log('Results from getMultiplePostsPromise:-\n', results)
-//   console.log('GET multiple posts with Promise END')
-// }
-// getMultiplePostsPromise()
+const simulateNotFoundError = async () => {
+  console.log('simulateNotFoundError START')
+  got.get('posts/abc', impl.options())
+    .then((result) => {
+      console.log(result)
+    })
+    .catch((err) => {
+      handler.error('posts/abc', 'GET', err)
+    })
+    .finally(() => {
+      console.log('simulateNotFoundError END')
+    })
+}
+simulateNotFoundError()
 
-// // const simulateServerError = async () => {
-// //   console.log('simulateServerError - This might take a while...')
-// //   got.put('postz', impl.options())
-// //   .catch((err) => {
-// //     console.log(err)
-// //     handler.error('postz', 'PUT',  err)
-// //   })
-// // }
-// // simulateServerError()
-
-// const simulateNotFoundError = async () => {
-//   console.log('simulateNotFoundError START')
-//   got.get('posts/abc', impl.options())
-//     .then((result) => {
-//       console.log(result)
-//     })
-//     .catch((err) => {
-//       handler.error('posts/abc', 'GET', err)
-//     })
-//     .finally(() => {
-//       console.log('simulateNotFoundError END')
-//     })
-// }
-// simulateNotFoundError()
-
-// const simulateTimeoutError = async () => {
-//   console.log('simulateTimeoutError START')
-//   got.get('users?delay=3', impl.timeout(REQ_BASE_URL, 1000))
-//     .json()
-//     .then((result) => {
-//       console.log('result', result)
-//     })
-//     .catch((err) => {
-//       handler.error('api/users?delay=3', 'GET', err)
-//     })
-// }
-// simulateTimeoutError()
+const simulateTimeoutError = async () => {
+  console.log('simulateTimeoutError START')
+  got.get('users?delay=3', impl.timeout(REQ_BASE_URL, 1000))
+    .json()
+    .then((result) => {
+      console.log('result', result)
+    })
+    .catch((err) => {
+      handler.error('api/users?delay=3', 'GET', err)
+    })
+}
+simulateTimeoutError()
 
 const simulateDomainNotFound = async () => {
   console.log('simulateDomainNotFound START')
